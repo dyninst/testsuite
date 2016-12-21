d1e5195e3c5126f1d3bccd372e47c39198dba18a
Itaru Kitayama <itaru.kitayama@riken.jp>
2016-12-20 18:48:05 -0600

    Remove test_symtab_ser_funcs test from the set of SymtabAPI tests

--- aarch64-unknown-linux/symtab_group_test_group.c
+++ aarch64-unknown-linux/symtab_group_test_group.c
@@ -10,7 +10,6 @@ extern int test_lookup_var_mutatee();
 extern int test_line_info_mutatee();
 extern int test_relocations_mutatee();
 extern int test_add_symbols_mutatee();
-extern int test_symtab_ser_funcs_mutatee();
 extern int test_ser_anno_mutatee();
 
 mutatee_call_info_t mutatee_funcs[] = {
@@ -21,7 +20,6 @@ mutatee_call_info_t mutatee_funcs[] = {
   {"test_line_info", test_line_info_mutatee, GROUPED, "test_line_info"},
   {"test_relocations", test_relocations_mutatee, GROUPED, "test_relocations"},
   {"test_add_symbols", test_add_symbols_mutatee, GROUPED, "test_add_symbols"},
-  {"test_symtab_ser_funcs", test_symtab_ser_funcs_mutatee, GROUPED, "test_symtab_ser_funcs"},
   {"test_ser_anno", test_ser_anno_mutatee, GROUPED, "test_ser_anno"}
 };
 
