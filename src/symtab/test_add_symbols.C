915df437a6e3e78499491cd69ebe0373e5d2a278
Xiaozhu Meng <xmeng@cs.wisc.edu>
2019-02-18 17:49:36 -0600

    Fix test_add symbols test for PIC mode.
    For PIE binaries (including libraries), Dyninst need to shift
    the address space by 1 page. Fix the test to consider this effect.

--- src/symtab/test_add_symbols.C
+++ src/symtab/test_add_symbols.C
@@ -167,7 +167,10 @@ test_results_t test_add_symbols_Mutator::executeTest()
     logerror("[%s:%u]: got unexpected number of symbols matching add_sym_newsymbol: %d != 1\n", __FILE__, __LINE__, lookupSyms.size());
     return FAILED;
   }
-  if (lookupSyms[0]->getOffset() != syms[0]->getOffset()) {
+  unsigned libraryAdjust = 0;
+  if (newSymtab->getObjectType() == obj_SharedLib)
+    libraryAdjust = getpagesize();
+  if (lookupSyms[0]->getOffset() != libraryAdjust + syms[0]->getOffset()) {
     logerror("[%s:%u]: added symbol offset 0x%lx not equal to expected 0x%lx\n", __FILE__, __LINE__,
 	     lookupSyms[0]->getOffset(), syms[0]->getOffset());
     return FAILED;
