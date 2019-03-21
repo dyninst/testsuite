ee2cfa2bc1b7a6fe8f33b7f49dae401e1c7b1a2f
Tim Haines <thaines.astro@gmail.com>
2019-03-21 14:51:19 -0500

    Add log parsing
    This parses the standard output from the test executions into a CSV file
    suitable for importing into a database.

--- build.pl
+++ build.pl
@@ -179,19 +179,17 @@ sub execute($) {
 }
 
 sub parse_log {
-#die "Usage: $0 in.log out.res\n" unless @ARGV == 2;
-#
-#open my $fdIn, '<', $ARGV[0] or die "$!\n";
-#open my $fdOut, '>', $ARGV[1] or die "$!\n";
-#while(<$fdIn>) {
-#	chomp;
-#	my @x = unpack('a27 a7 a5 a4 a9 a8 a8 a8 a23');
-#	my $status = pop @x;
-#	$status = (split(' ', $status))[0];
-#	push @x, $status;
-#	my @line = map {s/\s+//g; $_;} @x;
-#	print $fdOut join(',', @line), "\n";
-#}
+	open my $fdIn, '<', $_[0] or die "$_[0]: $!\n";
+	my @results;
+	while(<$fdIn>) {
+		chomp;
+		my @x = unpack('a27 a7 a5 a4 a9 a8 a8 a8 a23');
+		my $status = pop @x;
+		$status = (split(' ', $status))[0];
+		push @x, $status;
+		push @results, map {s/\s+//g; $_;} @x;
+	}
+	return @results;
 }
 __END__
 
