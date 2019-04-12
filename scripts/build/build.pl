e8cbe4698b46cae86e56e886e1253bc603f11f6b
Tim Haines <thaines.astro@gmail.com>
2019-04-11 19:25:26 -0500

    Remove parse_log

--- scripts/build/build.pl
+++ scripts/build/build.pl
@@ -391,19 +391,6 @@ sub execute($) {
 	return $stdout;
 }
 
-sub parse_log {
-	open my $fdIn, '<', $_[0] or die "$_[0]: $!\n";
-	my @results;
-	while(<$fdIn>) {
-		chomp;
-		my @x = unpack('a27 a7 a5 a4 a9 a8 a8 a8 a23');
-		my $status = pop @x;
-		$status = (split(' ', $status))[0];
-		push @x, $status;
-		push @results, map {s/\s+//g; $_;} @x;
-	}
-	return @results;
-}
 __END__
 
 =head1 DESCRIPTION
