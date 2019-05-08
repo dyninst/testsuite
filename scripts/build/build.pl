ec0f33a99fa1c07e61131bb156996fcdc019acc5
Tim Haines <thaines.astro@gmail.com>
2019-05-08 16:26:01 -0500

    Prepend user-specified paths to LD_LIBRARY_PATH when running Testsuite (#56)

--- scripts/build/build.pl
+++ scripts/build/build.pl
@@ -419,7 +419,7 @@ sub run_tests {
 		execute(
 			"cd $base_dir\n" .
 			"export DYNINSTAPI_RT_LIB=$base_dir/../dyninst/lib/libdyninstAPI_RT.so\n".
-			"LD_LIBRARY_PATH=$paths " .
+			"LD_LIBRARY_PATH=$paths:\$LD_LIBRARY_PATH " .
 			"./runTests -all -log test.log 1>stdout.log 2>stderr.log"
 		);
 	};
