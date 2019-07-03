1d6f727122151d5aeef5ae918cf6e41141e4693e
Tim Haines <thaines.astro@gmail.com>
2019-07-03 10:16:42 -0500

    Change directory before doing git merge for a PR in build script (#78)

--- scripts/build/Dyninst/git.pm
+++ scripts/build/Dyninst/git.pm
@@ -76,7 +76,10 @@ sub checkout_pr {
 				# Running 'git commit' with nothing to commit returns a non-zero value.
 				# I think this is a bug in git, but just work around it for now.
 				if($stdout !~ /Already up-to-date/i) {
-					execute("git commit -m 'Merge $remote/master'");
+					execute(
+						"cd $src_dir \n" .
+						"git commit -m 'Merge $remote/master'"
+					);
 				}
 			}
 		}
