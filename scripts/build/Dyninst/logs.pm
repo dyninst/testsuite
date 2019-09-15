dyninst/testsuite

commit 353f98d414d7adeac9c64123d1ef796d71294a5d

Add token-based authentication to dashboard upload (#97)

scripts/build/Dyninst/logs.pm
@@ -68,6 +68,9 @@ sub save_system_info {
 	# Strip trailing digits from the hostname (these are usually from login nodes)
 	$nodename =~ s/\d+$//;
 	
+	# Save the hostname so the caller can use it
+	$args{'hostname'} = $nodename;
+	
 	# Try to get the vendor name
 	my $vendor_name = 'unknown';
 	

scripts/build/build.pl
@@ -250,8 +250,15 @@ if($args{'purge'}) {
 
 # Upload the results to the dashboard, if requested
 if($args{'upload'}) {
+	use Digest::MD5 qw(md5_hex);
+	my $token = md5_hex($args{'hostname'});
+	
 	eval {
-		Dyninst::utils::execute("curl -F upload=\@$root_dir.results.tar.gz https://bottle.cs.wisc.edu/upload");
+		Dyninst::utils::execute(
+			"curl -F \"upload=\@$root_dir.results.tar.gz\" ".
+			"-F \"token=$token\" ".
+			"https://bottle.cs.wisc.edu/upload"
+		);
 	};
 	if($@) {
 		print "An error occurred when uploading the results\n$@\n";

commit 1684a60561f04b2b02faa8349561ef1ac2ddc2f3

Correctly save hostname in build script (#98)
This fixes a bug introduced in #97.

scripts/build/Dyninst/logs.pm
@@ -69,7 +69,7 @@ sub save_system_info {
 	$nodename =~ s/\d+$//;
 	
 	# Save the hostname so the caller can use it
-	$args{'hostname'} = $nodename;
+	$args->{'hostname'} = $nodename;
 	
 	# Try to get the vendor name
 	my $vendor_name = 'unknown';