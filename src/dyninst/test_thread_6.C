763a3aa2e8e0c99780f3a1bcb29996809f9704ba
Tim Haines <thaines.astro@gmail.com>
2020-06-19 19:41:38 -0500

    Add debug logging of thread counts

--- src/dyninst/test_thread_6.C
+++ src/dyninst/test_thread_6.C
@@ -204,6 +204,7 @@ test_results_t test_thread_6_Mutator::mutatorTest(BPatch *bpatch) {
   {
     std::vector<BPatch_thread *> threads;
     appProc->getThreads(threads);
+    dprintf("Found %z mutator threads\n", threads.size());
     for (auto *t : threads) {
       if (t == appThread)
         continue;
@@ -236,6 +237,7 @@ test_results_t test_thread_6_Mutator::mutatorTest(BPatch *bpatch) {
   {
     BPatch_Vector<BPatch_thread *> thrds;
     mutatee_process->getThreads(thrds);
+    dprintf("Found %z mutatee threads\n", thrds.size());
     if (thrds.size() != NUM_THREADS) {
       dprintf("[%s:%d] - Have %u threads, expected %u!\n", __FILE__, __LINE__,
               thrds.size(), NUM_THREADS);
