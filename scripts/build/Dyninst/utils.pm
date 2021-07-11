2756fc64cd67b3b7931af1713186000c30c9e0df
Tim Haines <thaines@cs.wisc.edu>
2021-07-11 17:15:14 -0500

    Whitespace cleanup

--- scripts/build/Dyninst/utils.pm
+++ scripts/build/Dyninst/utils.pm
@@ -20,8 +20,8 @@ sub execute {
 
 	print "\n$cmd\n" if $debug_mode;
 
-	my ($stdout,$stderr,$exit) = capture { system($cmd); };
-	$exit = (( $exit >> 8 ) != 0 || $exit == -1 || ( $exit & 127 ) != 0);
+	my ($stdout, $stderr, $exit) = capture { system($cmd); };
+	$exit = (($exit >> 8) != 0 || $exit == -1 || ($exit & 127) != 0);
 	die "Error executing '$cmd'\n$stderr\n" if $exit;
 	return $stdout;
 }
@@ -34,10 +34,10 @@ sub list_unique {
 
 sub parse_cmake_cache {
 	my $filename = shift;
-	my %defines = ();
+	my %defines  = ();
 
 	open my $fdIn, '<', $filename or die "Unable to open CMake cache '$filename': $!\n";
-	while(<$fdIn>) {
+	while (<$fdIn>) {
 		chomp;
 		next if /^#/;
 		next if /^\/\//;
@@ -59,49 +59,49 @@ sub load_from_cache {
 
 sub save_compiler_config {
 	my ($cmake_log, $out_file) = @_;
-	
+
 	open my $fdIn, '<', $cmake_log or die "Unable to open CMake log '$cmake_log': $!\n";
-	
+
 	my %compilers = (
-		'cxx' => {'path'=>'', 'version'=>''},
-		'c'   => {'path'=>'', 'version'=>''}
+		'cxx' => { 'path' => '', 'version' => '' },
+		'c'   => { 'path' => '', 'version' => '' }
 	);
 
-	while(<$fdIn>) {
-		if(/Check for working CXX compiler:\s*(.+)?\s*[-]+\s*works/) {
+	while (<$fdIn>) {
+		if (/Check for working CXX compiler:\s*(.+)?\s*[-]+\s*works/) {
 			$compilers{'cxx'}{'path'} = realpath($1);
 			next;
 		}
-		if(/Check for working C compiler:\s*(.+)?\s*[-]+\s*works/) {
+		if (/Check for working C compiler:\s*(.+)?\s*[-]+\s*works/) {
 			$compilers{'c'}{'path'} = realpath($1);
 			next;
 		}
-		if(/The C compiler identification is (.+)/) {
+		if (/The C compiler identification is (.+)/) {
 			$compilers{'c'}{'version'} = $1;
 			next;
 		}
-		if(/The CXX compiler identification is (.+)/) {
+		if (/The CXX compiler identification is (.+)/) {
 			$compilers{'cxx'}{'version'} = $1;
 			next;
 		}
 	}
-	
+
 	# Verify we got everything
 	for my $c (keys %compilers) {
-		for my $t (keys %{$compilers{$c}}) {
-			unless($compilers{$c}{$t}) {
+		for my $t (keys %{ $compilers{$c} }) {
+			unless ($compilers{$c}{$t}) {
 				warn "$cmake_log is missing $c/$t\n";
 			}
 		}
 	}
-	
+
 	open my $fdOut, '>', $out_file or die "Couldn't open '$out_file': $!\n";
 	local $, = "\n";
 	print $fdOut
-		"c_path: $compilers{'c'}{'path'}",
-		"c_version: $compilers{'c'}{'version'}",
-		"cxx_path: $compilers{'cxx'}{'path'}",
-		"cxx_version: $compilers{'cxx'}{'version'}\n";
+	  "c_path: $compilers{'c'}{'path'}",
+	  "c_version: $compilers{'c'}{'version'}",
+	  "cxx_path: $compilers{'cxx'}{'path'}",
+	  "cxx_version: $compilers{'cxx'}{'version'}\n";
 }
 
 sub make_root {
