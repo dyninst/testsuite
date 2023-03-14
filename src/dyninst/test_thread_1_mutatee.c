69aae230d38636eea32909f6b91852f70bb51239
Tim Haines <thaines.astro@gmail.com>
2023-03-14 09:40:19 -0500

    Whitespace

--- src/dyninst/test_thread_1_mutatee.c
+++ src/dyninst/test_thread_1_mutatee.c
@@ -1,42 +1,43 @@
 /*
  * See the dyninst/COPYRIGHT file for copyright information.
- * 
+ *
  * We provide the Paradyn Tools (below described as "Paradyn")
  * on an AS IS basis, and do not warrant its validity or performance.
  * We reserve the right to update, modify, or discontinue this
  * software at any time.  We shall have no obligation to supply such
  * updates or modifications or any other form of support to you.
- * 
+ *
  * By your use of Paradyn, you understand and agree that we (or any
  * other person or entity with proprietary rights in Paradyn) are
  * under no obligation to provide either maintenance services,
  * update services, notices of latent defects, or correction of
  * defects for Paradyn.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
 #include <dlfcn.h>
 #include <pthread.h>
 #include <stdlib.h>
 
+#include "atomic.h"
+#include "dyninstRTExport.h"
 #include "mutatee_util.h"
 #include "solo_mutatee_boilerplate.h"
-#include "test_thread.h"
 #include "test12.h"
-#include "dyninstRTExport.h"
-#include "atomic.h"
+#include "test_thread.h"
 
 /********************************************************************
 Subtest 1:  rtlib spinlocks
@@ -64,35 +65,34 @@ testsuite_atomic(pthread_t, canary, 0)
  */
 static pthread_barrier_t startup_barrier;
 
-static void *thread_main1 (void *arg)
-{
-   pthread_barrier_wait(&startup_barrier);
+static void *thread_main1(void *arg) {
+  pthread_barrier_wait(&startup_barrier);
 
-   // We need a unique value for each thread, so just use its pthread ID
-   pthread_t id = pthread_self();
+  // We need a unique value for each thread, so just use its pthread ID
+  pthread_t id = pthread_self();
 
-   (*DYNINSTlock_thelock)(&test1lock);
+  (*DYNINSTlock_thelock)(&test1lock);
 
-   // This is atomic
-   canary = id;
+  // This is atomic
+  canary = id;
 
-   // Introduce some noise
-   pthread_yield();
+  // Introduce some noise
+  pthread_yield();
 
-   if(canary != id) subtest1err = 1;
+  if (canary != id)
+    subtest1err = 1;
 
-   canary = id;
-   pthread_yield();
+  canary = id;
+  pthread_yield();
 
-   (*DYNINSTunlock_thelock)(&test1lock);
+  (*DYNINSTunlock_thelock)(&test1lock);
 
-   return arg;
+  return arg;
 }
 
-int func1_1()
-{
+int func1_1() {
 #if defined(m32_test)
-  const char * libname = "libdyninstAPI_RT_m32.so";
+  const char *libname = "libdyninstAPI_RT_m32.so";
 #else
   const char *libname = "libdyninstAPI_RT.so";
 #endif
@@ -138,8 +138,8 @@ int func1_1()
   dprintf("%s[%d]:  doing initial unlock...\n", __FILE__, __LINE__);
   (*DYNINSTunlock_thelock)(&test1lock);
 
-  for(int i=0; i<TEST1_THREADS; i++) {
-	pthread_join(test1threads[i], NULL);
+  for (int i = 0; i < TEST1_THREADS; i++) {
+    pthread_join(test1threads[i], NULL);
   }
 
   dlclose(RTlib);
