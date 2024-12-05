70d09bcdf2194cdc661aca9e782f9bb9829af33c
Tim Haines <thaines.astro@gmail.com>
2024-12-05 10:55:44 -0600

    Fix possible double-free in CFT tests (#240)
    In the event there are more operands than test results, the test should fail, not crash.

--- src/instruction/aarch64_cft.C
+++ src/instruction/aarch64_cft.C
@@ -199,7 +199,9 @@ test_results_t aarch64_cft_Mutator::executeTest()
               logerror("FAILED: instruction %s expected CFT, wasn't present", decodedInsns.front().format().c_str());
               retVal = failure_accumulator(retVal, FAILED);
           }
-          cfts.pop_front();
+          if(!cfts.empty()) {
+            cfts.pop_front();
+          }
       }
 
       decodedInsns.pop_front();
